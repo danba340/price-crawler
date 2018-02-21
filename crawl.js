2703a265f8b72f5cf43279069b78f4fa37874ae7
danba340 <danba340@gmail.com>
2018-02-21 22:31:35 +0100

    Reduce fix

--- crawl.js
+++ crawl.js
@@ -10,10 +10,10 @@ const BASE_URL = 'https://www.prisjakt.nu/ajax/server.php?class=Graph_Product&me
 
 const opts = {
     headers: {
-        cookie: 'usersettings=%7B%22sidebar_layout_mode%22%3A%22hidden%22%2C%22active_sidebar_list%22%3A%22MyLists%22%2C%22filter_layout_mode%22%3A%22maximized%22%2C%22category_matrix_layout%22%3A%22img%22%2C%22category_realtime_search%22%3A%221%22%2C%22category_nav_layout%22%3A%22list%22%2C%22products_per_page%22%3A%22100%22%2C%22products_per_page_mixed%22%3A1000%2C%22products_per_page_image%22%3A%221000%22%2C%22mobile_products_per_page%22%3A%221000%22%2C%22mobile_products_per_page_mixed%22%3A%221000%22%2C%22mobile_products_per_page_image%22%3A%2230%22%7D'
+        cookie: 'usersettings=%7B%22sidebar_layout_mode%22%3A%22hidden%22%2C%22active_sidebar_list%22%3A%22MyLists%22%2C%22filter_layout_mode%22%3A%22maximized%22%2C%22category_matrix_layout%22%3A%22img%22%2C%22category_realtime_search%22%3A%221%22%2C%22category_nav_layout%22%3A%22list%22%2C%22products_per_page%22%3A%22100%22%2C%22products_per_page_mixed%22%3A100%2C%22products_per_page_image%22%3A%221000%22%2C%22mobile_products_per_page%22%3A%221000%22%2C%22mobile_products_per_page_mixed%22%3A%221000%22%2C%22mobile_products_per_page_image%22%3A%2230%22%7D'
     }
 };
-fetch(`https://www.prisjakt.nu/kategori.php?k=1381`, opts)
+fetch(`https://www.prisjakt.nu/kategori.php?k=148`, opts)
 .then(res => res.text())
 .then(body => {
     let $ = cheerio.load(body);
@@ -27,9 +27,10 @@ fetch(`https://www.prisjakt.nu/kategori.php?k=1381`, opts)
             let product = {};
             product.id = id;
             if( parseInt(json.items[0][0].value) < 1 || json.items[0][json.items[0].length-1].value < 1 ) return;
-            let pricesSum = json.items[0].reduce(function(a,b){
-                return a + b;
-            });
+            function add(a, b) {
+                return a + b.value;
+            }
+            let pricesSum = json.items[0].reduce(add, 0);
             product.avgPrice = pricesSum / json.items[0].length
             product.pricePercentDrop = json.items[0][json.items[0].length-1].value / product.avgPrice; //json.items[0][0].value;
             productsWithPriceDiff.push(product);
